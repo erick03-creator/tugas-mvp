14006b82f3e1bf8bc6bb484459ae6694d9f1e2d5
Erick Sugiantoo <er1ckk4run4put4@gmail.com>
2026-09-18 21:30:09 +0800

    Initial project Vue Ionic Capacitor

---
+++ README.md
@@ -0,0 +1,50 @@
+# Tugas MVP - Aplikasi To-Do
+
+## Deskripsi
+
+Aplikasi To-Do sederhana yang dibuat menggunakan Vue 3,
+Ionic, dan Capacitor.
+
+Aplikasi ini membantu mahasiswa mencatat tugas kuliah
+yang perlu dikerjakan.
+
+## Masalah
+
+Mahasiswa sering memiliki beberapa tugas perkuliahan
+yang harus dikerjakan. Tanpa pencatatan yang baik,
+tugas dapat terlupa.
+
+Aplikasi ini dibuat sebagai solusi sederhana untuk
+mencatat tugas kuliah.
+
+## Target Pengguna
+
+Target pengguna aplikasi adalah mahasiswa yang ingin
+mencatat dan mengingat tugas perkuliahan mereka.
+
+## Scope MVP
+
+Fitur yang termasuk dalam MVP:
+
+- Menampilkan halaman utama
+- Menampilkan Hello World
+- Menambahkan tugas
+- Menampilkan daftar tugas
+- Menggunakan komponen Ionic
+- Menjalankan aplikasi menggunakan Capacitor
+
+## Teknologi
+
+- Vue 3
+- Ionic Vue
+- Capacitor
+- Vite
+- JavaScript
+
+## Fitur yang Belum Termasuk
+
+- Login
+- Database online
+- Notifikasi
+- Sinkronisasi antar perangkat
+- Berbagi tugas dengan pengguna lain
---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <ion-app>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tugas MVP</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <h1>Hello World 👋</h1>
+
+      <p>
+        Aplikasi To-Do sederhana untuk membantu mahasiswa
+        mencatat tugas kuliah.
+      </p>
+
+      <ion-item>
+        <ion-input
+          v-model="task"
+          placeholder="Masukkan tugas"
+        ></ion-input>
+      </ion-item>
+
+      <ion-button expand="block" @click="addTask">
+        Tambah Tugas
+      </ion-button>
+
+      <ion-list>
+        <ion-item
+          v-for="(item, index) in tasks"
+          :key="index"
+        >
+          {{ item }}
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import {
+  IonApp,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonList
+} from '@ionic/vue'
+
+const task = ref('')
+const tasks = ref([])
+
+function addTask() {
+  if (task.value.trim() !== '') {
+    tasks.value.push(task.value)
+    task.value = ''
+  }
+}
+</script>
