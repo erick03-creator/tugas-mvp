ed639848ddd00aec028fd474488939708ba7fcdb
Erick Sugiantoo <er1ckk4run4put4@gmail.com>
2026-09-18 22:18:05 +0800

    Simplify Hello World page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <ion-app>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tugas MVP</ion-title>
+        <ion-title>Hello World</ion-title>
       </ion-toolbar>
     </ion-header>
 
@@ -10,55 +10,18 @@
       <h1>Hello World 👋</h1>
 
       <p>
-        Aplikasi To-Do sederhana untuk membantu mahasiswa
-        mencatat tugas kuliah.
+        Aplikasi ini dibuat menggunakan Vue 3, Ionic, dan Capacitor.
       </p>
-
-      <ion-item>
-        <ion-input
-          v-model="task"
-          placeholder="Masukkan tugas"
-        ></ion-input>
-      </ion-item>
-
-      <ion-button expand="block" @click="addTask">
-        Tambah Tugas
-      </ion-button>
-
-      <ion-list>
-        <ion-item
-          v-for="(item, index) in tasks"
-          :key="index"
-        >
-          {{ item }}
-        </ion-item>
-      </ion-list>
     </ion-content>
   </ion-app>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
 import {
   IonApp,
   IonHeader,
   IonToolbar,
   IonTitle,
-  IonContent,
-  IonItem,
-  IonInput,
-  IonButton,
-  IonList
+  IonContent
 } from '@ionic/vue'
-
-const task = ref('')
-const tasks = ref([])
-
-function addTask() {
-  if (task.value.trim() !== '') {
-    tasks.value.push(task.value)
-    task.value = ''
-  }
-}
 </script>
